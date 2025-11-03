bc0bbb5e9270325c136714db6bd1b532a0a6a24c
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-03 20:02:26 +0100

    Delete JSON data structure from test.java
    Remove JSON object from test.java

--- test.java
+++ test.java
@@ -1,17 +1,1 @@
 
-{
-  "requestId": "123456",
-  "status": "READY",
-  "datasets": [
-    {
-      "dataSetName": "family_name",
-      "dataSetType": "string",
-      "value": "DUPONT"
-    },
-    {
-      "dataSetName": "birth_date",
-      "dataSetType": "date",
-      "value": "1985-04-12"
-    }
-  ]
-}
