2f1d932cd5498bae3155334969a06d49bf421797
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-03 20:01:09 +0100

    Add JSON data structure to test.java

--- test.java
+++ test.java
@@ -1,1 +1,17 @@
 
+{
+  "requestId": "123456",
+  "status": "READY",
+  "datasets": [
+    {
+      "dataSetName": "family_name",
+      "dataSetType": "string",
+      "value": "DUPONT"
+    },
+    {
+      "dataSetName": "birth_date",
+      "dataSetType": "date",
+      "value": "1985-04-12"
+    }
+  ]
+}
