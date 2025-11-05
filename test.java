99e25601e8628d04b783a7ed18406fc725e53dcd
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-05 15:35:35 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,42 +1,6 @@
-package com.bnpp.pf.walle.access.config;
-
-import com.bnpp.pf.walle.access.service.JwtUtil;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.*;
-import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-@Component
-@RequiredArgsConstructor
-public class JwtRequestInterceptor implements ClientHttpRequestInterceptor {
-
-    private final JwtUtil jwtUtil;
-
-    // Nom du header "spécial" pour ignorer l’ajout automatique
-    private static final String SKIP_JWT_HEADER = "X-Skip-JWT";
-
-    @Override
-    public ClientHttpResponse intercept(
-            HttpRequest request,
-            byte[] body,
-            ClientHttpRequestExecution execution
-    ) throws IOException {
-
-        HttpHeaders headers = request.getHeaders();
-
-        // Si le header "X-Skip-JWT" est présent, ne pas ajouter le JWT
-        if (!headers.containsKey(SKIP_JWT_HEADER)) {
-            String jwt = jwtUtil.generateJwt();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.setBearerAuth(jwt);
-        } else {
-            // Supprime ce header interne pour qu'il ne parte pas vers le serveur
-            headers.remove(SKIP_JWT_HEADER);
-        }
-
-        return execution.execute(request, body);
-    }
+```json
+{
+  "clé": "valeur",
+  "nombre": 123
 }
+```
