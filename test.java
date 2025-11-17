f2b595551bcfcc22c3bf9bc655a752265be9a6a1
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-17 12:31:54 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,2 @@
-.replaceAll("-----BEGIN [^-]+-----", "")
-.replaceAll("-----END [^-]+-----", "")
-.replaceAll("\\s", "");
+openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt \
+  -in key.pem -out key_pkcs8.pem
