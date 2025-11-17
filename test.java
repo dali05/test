8d74466cce5bea29563a6ac673b8672428a86eac
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-17 12:34:56 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,2 +1,8 @@
-openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt \
-  -in key.pem -out key_pkcs8.pem
+String key = Files.readString(Paths.get("src/main/resources/cert/key_pkcs8.pem"))
+        .replace("-----BEGIN PRIVATE KEY-----", "")
+        .replace("-----END PRIVATE KEY-----", "")
+        .replaceAll("\\s+", "");
+
+byte[] keyBytes = Base64.getDecoder().decode(key);
+PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+RSAPrivateKey privateKey = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(keySpec);
