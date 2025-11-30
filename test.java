a42e10ff645a55caa8ceb2593442d2982a687f04
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-30 10:05:15 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,20 +1,7 @@
-spring:
-  jpa:
-    ...
-h2:
-  console:
-    enabled: true
-    path: /h2-console
-
-zeebe:
-  client:
-    gateway-url: http://localhost:26500
-    prefer-rest-over-grpc: false   # très important sinon le client essaie d’utiliser le REST endpoint !
-
-camunda:
-  client:
-    operate:
-      base-url: http://localhost:8081
-      username: demo
-      password: demo
-      auth-enabled: false
+{
+  "bpmnProcessId": "create_request",
+  "variables": {
+    "client_code": "12345",
+    "request_id": "REQ001"
+  }
+}
