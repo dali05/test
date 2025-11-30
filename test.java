b3ce0c5bb0d89ec0372d49c826bc3ec89a7f9bee
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-30 08:49:51 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,16 +1,14 @@
-<plugin>
-    <groupId>org.apache.maven.plugins</groupId>
-    <artifactId>maven-enforcer-plugin</artifactId>
-    <executions>
-        <execution>
-            <id>pf-enforce-ban-log-provider</id>
-            <phase>validate</phase>
-            <goals>
-                <goal>enforce</goal>
-            </goals>
-            <configuration>
-                <skip>true</skip>   <!-- ⬅️ IGNORER LA RÈGLE -->
-            </configuration>
-        </execution>
-    </executions>
-</plugin>
+camunda:
+  client:
+    mode: self-managed          # ← ajoute / corrige cette ligne
+    zeebe:
+      request-timeout: 360s
+      base-url: ${ZEEBE_BASE_URL:http://localhost:8080}
+      gateway-url: ${ZEEBE_GATEWAY_URL:localhost:26500}
+      security:
+        plaintext: true         # ← force une connexion non-TLS si dispo dans ta version
+    operate:
+      base-url: ${OPERATE_BASE_URL:http://localhost:8081}
+      username: demo
+      password: demo
+      auth-enabled: false
