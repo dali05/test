cf43d61dc97f582fcd5b90b45ea981679e373763
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-30 09:25:08 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,9 +1,20 @@
+spring:
+  jpa:
+    ...
+h2:
+  console:
+    enabled: true
+    path: /h2-console
+
+zeebe:
+  client:
+    gateway-url: http://localhost:26500
+    prefer-rest-over-grpc: false   # très important sinon le client essaie d’utiliser le REST endpoint !
+
 camunda:
   client:
-    mode: cluster
-    cluster:
-      zeebe:
-        gateway:
-          address: localhost:26500
-        security:
-          plaintext: true
+    operate:
+      base-url: http://localhost:8081
+      username: demo
+      password: demo
+      auth-enabled: false
