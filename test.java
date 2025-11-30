a8bb569a1304f4367b85c1c5788fc258af04ad0b
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-30 01:55:57 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,28 +1,19 @@
-version: "3.9"
+version: "3.7"
 services:
-  zeebe:
-    image: camunda/zeebe:8.6.0
-    ports:
-      - "26500:26500"
-      - "9600:9600"
-    environment:
-      - ZEEBE_LOG_LEVEL=info
-
-  operate:
-    image: camunda/operate:8.6.0
-    ports:
-      - "8081:8080"
-    environment:
-      - CAMUNDA_OPERATE_ZEEBE_BROKER_GATEWAYADDRESS=zeebe:26500
-
-  tasklist:
-    image: camunda/tasklist:8.6.0
-    ports:
-      - "8082:8080"
-
   elasticsearch:
-    image: docker.elastic.co/elasticsearch/elasticsearch:8.11.0
+    image: docker.elastic.co/elasticsearch/elasticsearch:8.14.0
+    container_name: elasticsearch
     environment:
       - discovery.type=single-node
+      - ES_JAVA_OPTS=-Xms1g -Xmx1g
     ports:
       - "9200:9200"
+      - "9300:9300"
+
+  kibana:
+    image: docker.elastic.co/kibana/kibana:8.14.0
+    container_name: kibana
+    ports:
+      - "5601:5601"
+    environment:
+      - ELASTICSEARCH_HOSTS=http://elasticsearch:9200
