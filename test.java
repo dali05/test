6af9767bcf26994efddc8562e3f99a2b0b8f7a10
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-30 08:21:13 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,12 @@
-zeebe:
+camunda:
   client:
-    broker:
-      contactPoint: 127.0.0.1:26500
-    security:
-      plaintext: true
+    mode: simple
+
+    zeebe:
+      gateway-address: "localhost:26500"
+      security:
+        plaintext: true   # ⬅️ TRÈS IMPORTANT
+
+    operate:
+      base-url: "http://localhost:8081"
+      auth-enabled: false
