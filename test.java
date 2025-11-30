a245a433b7c706370b69b69bf1623ba685900895
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-30 02:16:41 +0100

    Update Docker Compose configuration for services

--- test.java
+++ test.java
@@ -1,19 +1,55 @@
-version: "3.7"
+ano docker-compose.yml
+
+
+version: '3.8'
+
 services:
+
+  zeebe:
+    image: camunda/zeebe:8.5.0
+    container_name: zeebe
+    environment:
+      ZEEBE_LOG_LEVEL: info
+    ports:
+      - "26500:26500" # Zeebe Gateway
+    volumes:
+      - zeebe_data:/usr/local/zeebe/data
+
+  operate:
+    image: camunda/operate:8.5.0
+    container_name: operate
+    environment:
+      - CAMUNDA_OPERATE_ZEEBE_BROKER_GATEWAYADDRESS=zeebe:26500
+      - CAMUNDA_OPERATE_ELASTICSEARCH_URL=http://elasticsearch:9200
+    ports:
+      - "8080:8080"
+    depends_on:
+      - zeebe
+      - elasticsearch
+
+  tasklist:
+    image: camunda/tasklist:8.5.0
+    container_name: tasklist
+    environment:
+      - CAMUNDA_TASKLIST_ZEEBE_BROKER_GATEWAYADDRESS=zeebe:26500
+      - CAMUNDA_TASKLIST_ELASTICSEARCH_URL=http://elasticsearch:9200
+    ports:
+      - "8081:8080"
+    depends_on:
+      - zeebe
+      - elasticsearch
+
   elasticsearch:
-    image: docker.elastic.co/elasticsearch/elasticsearch:8.14.0
+    image: docker.elastic.co/elasticsearch/elasticsearch:7.17.9
     container_name: elasticsearch
     environment:
       - discovery.type=single-node
-      - ES_JAVA_OPTS=-Xms1g -Xmx1g
+      - ES_JAVA_OPTS=-Xms512m -Xmx512m
     ports:
       - "9200:9200"
-      - "9300:9300"
+    volumes:
+      - es_data:/usr/share/elasticsearch/data
 
-  kibana:
-    image: docker.elastic.co/kibana/kibana:8.14.0
-    container_name: kibana
-    ports:
-      - "5601:5601"
-    environment:
-      - ELASTICSEARCH_HOSTS=http://elasticsearch:9200
+volumes:
+  zeebe_data:
+  es_data:
