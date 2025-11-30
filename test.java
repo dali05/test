f92271700f40b7587a620d24ae9dc40a4d8ae2a5
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-11-30 09:17:07 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,9 @@
 camunda:
   client:
-    mode: self-managed
-    zeebe:
-      gateway:
-        address: "localhost:26500"
-        plaintext: true
-    operate:
-      base-url: "http://localhost:8081"
-      auth:
-        enabled: false
+    mode: cluster
+    cluster:
+      zeebe:
+        gateway:
+          address: localhost:26500
+        security:
+          plaintext: true
