3f6f3feb98d72f1694771a413598debd6bd88445
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-01 13:20:20 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,20 +1,34 @@
-zeebe:
-  client:
-    connectionMode: ADDRESS        # on se connecte directement au gateway
-    default-tenant-id: default     # si tu n'utilises pas le multi-tenant
-    broker:
-      grpc-address: localhost:26500
-    security:
-      plaintext: true              # Camunda local fonctionne en non-TLS
-  worker:
-    threads: 10
-    max-jobs-active: 32
+<?xml version="1.0" encoding="UTF-8"?>
+<bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL"
+                  xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI"
+                  xmlns:dc="http://www.omg.org/spec/DD/20100524/DC"
+                  xmlns:zeebe="http://camunda.org/schema/zeebe/1.0"
+                  id="Definitions_1"
+                  targetNamespace="http://example.com/bpmn">
 
-camunda:
-  client:
-    operate:
-      base-url: http://localhost:8081
-      auth-enabled: false
-    tasklist:
-      base-url: http://localhost:8082
-      auth-enabled: false
+  <bpmn:process id="simpleProcess" name="Simple Process" isExecutable="true">
+
+    <!-- Start event -->
+    <bpmn:startEvent id="startEvent" name="Start">
+      <bpmn:outgoing>Flow_Start_To_Task</bpmn:outgoing>
+    </bpmn:startEvent>
+
+    <!-- Service Task handled by a Spring Boot Worker -->
+    <bpmn:serviceTask id="myServiceTask" name="Process Data">
+      <bpmn:incoming>Flow_Start_To_Task</bpmn:incoming>
+      <bpmn:outgoing>Flow_Task_To_End</bpmn:outgoing>
+      <zeebe:taskDefinition type="process-data" retries="3"/>
+    </bpmn:serviceTask>
+
+    <!-- End event -->
+    <bpmn:endEvent id="endEvent" name="End">
+      <bpmn:incoming>Flow_Task_To_End</bpmn:incoming>
+    </bpmn:endEvent>
+
+    <!-- Flows -->
+    <bpmn:sequenceFlow id="Flow_Start_To_Task" sourceRef="startEvent" targetRef="myServiceTask"/>
+    <bpmn:sequenceFlow id="Flow_Task_To_End" sourceRef="myServiceTask" targetRef="endEvent"/>
+
+  </bpmn:process>
+
+</bpmn:definitions>
