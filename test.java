3ee206dea77efec38dfbde90a1fa2642ba80eaf1
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-01 11:56:00 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,20 @@
-zeebe: client:
-connectionMode: ADDRESS
-default-tenant-id: ${ZEEBE TENANT ID)
-broker:
-grpc-address: SIZEEBE.
-GRPC ADDRESS)
-security:
-plaintext: false
-default-tenant-id: ${ZEEBE TENANT ID} worker:
-threads: $(ZEEBE WORKER THREADS: 10)
-max-jobs-active: ${ZEEBE WORKER MAX JOBS: 32}
+zeebe:
+  client:
+    connectionMode: ADDRESS        # on se connecte directement au gateway
+    default-tenant-id: default     # si tu n'utilises pas le multi-tenant
+    broker:
+      grpc-address: localhost:26500
+    security:
+      plaintext: true              # Camunda local fonctionne en non-TLS
+  worker:
+    threads: 10
+    max-jobs-active: 32
+
+camunda:
+  client:
+    operate:
+      base-url: http://localhost:8081
+      auth-enabled: false
+    tasklist:
+      base-url: http://localhost:8082
+      auth-enabled: false
