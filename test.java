fe1864b0debbbf742aae23a35a13d4d70fdaa7b6
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-01 11:55:45 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,24 +1,11 @@
-camunda:
-  client:
-    mode: saas|self-managed|legacy-zeebe  # <- tu mets self-managed si tu tournes en local
-    self-managed:
-      # === Zeebe Gateway ===
-      zeebe:
-        base-url: http://localhost:26500
-        enabled: true
-
-      # Utilisation du transport GRPC (par défaut)
-      prefer-rest-over-grpc: false
-
-      # === Security ===
-      # si ton zeebe local n’est pas sécurisé
-      auth:
-        enabled: false
-
-      # === Operate ===
-      operate:
-        base-url: http://localhost:8081
-        auth:
-          enabled: false
-          username: demo
-          password: demo
+zeebe: client:
+connectionMode: ADDRESS
+default-tenant-id: ${ZEEBE TENANT ID)
+broker:
+grpc-address: SIZEEBE.
+GRPC ADDRESS)
+security:
+plaintext: false
+default-tenant-id: ${ZEEBE TENANT ID} worker:
+threads: $(ZEEBE WORKER THREADS: 10)
+max-jobs-active: ${ZEEBE WORKER MAX JOBS: 32}
