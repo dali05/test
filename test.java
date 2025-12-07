7c0a7985625fd0613c8982cc1a8823ccb42251a2
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-07 19:18:51 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,32 @@
-Test
+@ExtendWith(MockitoExtension.class)
+class RequestWalletDataWorkerTest {
+
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private WebClient idactoWebClient;
+
+    private RequestWalletDataWorker worker;
+    private IdactoProperties props;
+
+    @BeforeEach
+    void setUp() {
+        props = new IdactoProperties();
+        props.setAuthorizationPath("/auth/token");
+
+        worker = Mockito.spy(new RequestWalletDataWorker(
+                idactoWebClient,
+                props,
+                null,
+                null,
+                new ObjectMapper()
+        ));
+
+        // On évite que loadTemplateJson() charge un vrai fichier
+        Mockito.doReturn(new ObjectMapper().createObjectNode())
+                .when(worker)
+                .loadTemplateJson();
+    }
+
+    @Test
     void getVpToken_shouldReturnToken_whenServiceResponds() {
 
         // GIVEN
