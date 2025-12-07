2bac02cb93ced5c477fdbcc8d38d2b1e293ba827
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-07 18:53:59 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,22 +1,11 @@
-@BeforeEach
-void setUp() {
+WebClient.RequestBodyUriSpec uriSpec = mock(WebClient.RequestBodyUriSpec.class);
+WebClient.RequestBodySpec bodySpec = mock(WebClient.RequestBodySpec.class);
+WebClient.RequestHeadersSpec headersSpec = mock(WebClient.RequestHeadersSpec.class);
+WebClient.ResponseSpec responseSpec = mock(WebClient.ResponseSpec.class);
 
-    props = new IdactoProperties();
-    props.setParseTokenPath("/parse/token");
-
-    // Create worker first
-    RequestWalletDataWorker raw = new RequestWalletDataWorker(
-            idactoWebClient,
-            props,
-            null,
-            null,
-            objectMapper
-    );
-
-    // Spy AFTER injection
-    worker = Mockito.spy(raw);
-
-    // Prevent real HTTP call in constructor
-    Mockito.doReturn(new ObjectMapper().createObjectNode())
-           .when(worker).loadTemplateJson();
-}
+when(webClient.post()).thenReturn(uriSpec);
+when(uriSpec.uri(anyString())).thenReturn(bodySpec);
+when(bodySpec.contentType(any())).thenReturn(bodySpec);
+when(bodySpec.bodyValue(any())).thenReturn(headersSpec);
+when(headersSpec.retrieve()).thenReturn(responseSpec);
+when(responseSpec.bodyToMono(any(Class.class))).thenReturn(Mono.just(...));
