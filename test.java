467cac18d8e2deff75aad5963df94206b073330f
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-08 20:30:08 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,36 @@
-@PostConstruct
-public void checkProxy() {
-    System.out.println("HTTP PROXY = " + System.getProperty("http.proxyHost") + ":" + System.getProperty("http.proxyPort"));
-    System.out.println("HTTPS PROXY = " + System.getProperty("https.proxyHost") + ":" + System.getProperty("https.proxyPort"));
-    System.out.println("HTTP USER  = " + System.getProperty("http.proxyUser"));
-    System.out.println("SYSTEM PROXY ENABLED = " + System.getProperty("java.net.useSystemProxies"));
+import io.netty.handler.proxy.HttpProxyHandler;
+import io.netty.resolver.NoopAddressResolverGroup;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.net.InetSocketAddress;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+
+        HttpClient httpClient = HttpClient.create()
+                // ⚠️ Empêche TOUTE résolution DNS locale
+                .resolver(NoopAddressResolverGroup.INSTANCE)
+                .tcpConfiguration(tcpClient ->
+                    tcpClient.doOnConnected(conn ->
+                        conn.addHandlerFirst(
+                                new HttpProxyHandler(
+                                        new InetSocketAddress("10.175.113.1", 3128),
+                                        "h93588",
+                                        "Bonjournp@2026"
+                                )
+                        )
+                    )
+                );
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
 }
