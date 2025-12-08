fcee236d3942c1fdf4fe06494588794c2f0b9b9a
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-08 20:22:09 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,12 +1,2 @@
--Dhttp.proxyHost=10.175.113.1
--Dhttp.proxyPort=3128
--Dhttps.proxyHost=10.175.113.1
--Dhttps.proxyPort=3128
-
--Dhttp.proxyUser=h93588
--Dhttp.proxyPassword=TON_MOT_DE_PASSE
-
--Dhttps.proxyUser=h93588
--Dhttps.proxyPassword=TON_MOT_DE_PASSE
-
--Dhttp.nonProxyHosts="localhost|*.echonet|*.xpf.net.intra"
+System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
+System.setProperty("jdk.http.auth.proxying.disabledSchemes", "");
