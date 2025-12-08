4745b2d927bbaecb278212a5797ee3e0a4b6cb44
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-08 20:27:19 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,2 +1,7 @@
-System.setProperty("jdk.http.auth.tunneling.disabledSchemes", "");
-System.setProperty("jdk.http.auth.proxying.disabledSchemes", "");
+@PostConstruct
+public void checkProxy() {
+    System.out.println("HTTP PROXY = " + System.getProperty("http.proxyHost") + ":" + System.getProperty("http.proxyPort"));
+    System.out.println("HTTPS PROXY = " + System.getProperty("https.proxyHost") + ":" + System.getProperty("https.proxyPort"));
+    System.out.println("HTTP USER  = " + System.getProperty("http.proxyUser"));
+    System.out.println("SYSTEM PROXY ENABLED = " + System.getProperty("java.net.useSystemProxies"));
+}
