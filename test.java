2ee2b891afad865053d54fe68ef67d33ef13a7eb
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-08 21:13:39 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,16 @@
-Field connectorField = webClient.getClass().getDeclaredField("connector");
-connectorField.setAccessible(true);
-System.out.println("Connector = " + connectorField.get(webClient));
+@Bean
+public HttpClient httpClient() {
+
+    return HttpClient.create()
+            // Désactive toute résolution DNS locale
+            .resolver(NoopAddressResolverGroup.INSTANCE)
+
+            // Définit le proxy AVANT l’ouverture de la connexion
+            .proxy(proxy -> proxy
+                    .type(ProxyProvider.Proxy.HTTP)
+                    .host("10.175.113.1")
+                    .port(3128)
+                    .username("h93588")
+                    .password(pass -> "Bonjournp@2026")
+            );
+}
