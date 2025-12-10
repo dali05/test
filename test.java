f0122ed6c7f2fadd054049d48ca6be0ad6450c68
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-11 00:27:38 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,22 @@
-eLHfrgT4lllhfHPy2edgjDgw5Td78SkNmV4-SRHkRR4
+zeebeClient
+    .newPublishMessageCommand()
+    .messageName("walletGetRequest4IdOK")
+    .correlationKey(requestId.toString())
+    .variables(Map.of(
+        "walletResponse", "OK",       // 💥 IMPORTANT : permet au workflow d'éviter la branche FEEL cassée
+        "requestRetrieved", true      // optionnel
+    ))
+    .send()
+    .join();
+
+
+zeebeClient
+    .newPublishMessageCommand()
+    .messageName("receivedResponseOK")
+    .correlationKey(requestId.toString())
+    .variables(Map.of(
+        "walletResponse", "OK",       // requis pour validation finale
+        "responseReceivedAt", System.currentTimeMillis()  // optionnel
+    ))
+    .send()
+    .join();
