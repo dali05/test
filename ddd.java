3a92a6052dafe52b2166ae68e96e7b0ad820472a
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-19 18:39:46 +0100

    Add Callback component for user login handling
    Implement user login callback handling with navigation.

--- ddd.java
+++ ddd.java
@@ -1,1 +1,22 @@
-ZggicmWqZD9mkXEbK_Z7y3qFa7laWgJMguFU7OHMRzc
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { userManager } from "./config";
+
+export default function Callback() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    userManager
+      .signinRedirectCallback()
+      .then(user => {
+        console.log("✅ User logged in", user);
+        navigate("/");
+      })
+      .catch(err => {
+        console.error("❌ Callback error", err);
+        navigate("/login");
+      });
+  }, [navigate]);
+
+  return <div>Connexion en cours...</div>;
+}
