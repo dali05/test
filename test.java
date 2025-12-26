5fa8f9219105c78d93947ef693d439432dec6e0f
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-26 02:02:56 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,20 @@
-ZggicmWqZD9mkXEbK_Z7y3qFa7laWgJMguFU7OHMRzc
+const oidcConfig: UserManagerSettings = {
+  authority: import.meta.env.VITE_OIDC_AUTHORITY,
+  client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_OIDC_REDIRECT_URI,
+  response_type: "code",
+  scope: import.meta.env.VITE_OIDC_SCOPE,
+  loadUserInfo: true,
+  automaticSilentRenew: false,
+  userStore: new WebStorageStateStore({ store: window.localStorage }),
+
+  onSigninCallback: () => {
+    window.history.replaceState(
+      {},
+      document.title,
+      window.location.pathname
+    );
+  },
+};
+
+
