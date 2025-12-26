a4fd729ebaf68ff56a4c9dd62fd5c02604978697
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2025-12-26 01:50:37 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -32,3 +32,15 @@ function App(): Element {
     </AuthProvider>
   );
 }
+
+
+const oidcConfig: UserManagerSettings = {
+  authority: import.meta.env.VITE_OIDC_AUTHORITY,
+  client_id: import.meta.env.VITE_OIDC_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_OIDC_REDIRECT_URI,
+  response_type: "code",
+  scope: import.meta.env.VITE_OIDC_SCOPE,
+  loadUserInfo: true,
+  automaticSilentRenew: false,
+  userStore: new WebStorageStateStore({ store: window.localStorage }),
+};
