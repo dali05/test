7eb2035a5056a3d6355620b66e01d74fae94f72e
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-23 00:44:38 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,11 +1,1 @@
--- Création du schéma applicatif
-CREATE SCHEMA IF NOT EXISTS admin;
-
--- Droits
-GRANT ALL ON SCHEMA admin TO admin;
-
--- Optionnel : schéma par défaut
-ALTER ROLE admin SET search_path TO admin, public;
-
-
 psql "$DATABASE_URL" -f /sql/init.sq
