f4947960a426f8c4305990bb2dfb0d70901a25a4
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-22 20:56:06 +0100

    Refactor database bootstrap job configuration
    Replaced old job configuration with a new Job resource definition for database bootstrap in Kubernetes.

--- ddd.java
+++ ddd.java
@@ -1,13 +1,25 @@
-job:
-  initcontainers:
-    enabled: true
-    createSchema:
-      command: >
-        sh -c "
-        echo 'Creating schema admin';
-        export PGPASSWORD=$POSTGRES_PASSWORD;
-        psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTGRES_DATABASE
-        -v ON_ERROR_STOP=1
-        -c 'CREATE SCHEMA IF NOT EXISTS admin';
-        echo 'Schema admin ready'
-        "
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: {{ .Release.Name }}-db-bootstrap
+  annotations:
+    "helm.sh/hook": pre-install,pre-upgrade
+    "helm.sh/hook-weight": "-10"
+    "helm.sh/hook-delete-policy": hook-succeeded
+spec:
+  template:
+    spec:
+      restartPolicy: Never
+      containers:
+        - name: bootstrap
+          image: postgres:15
+          env:
+            - name: DB_URL
+              valueFrom:
+                secretKeyRef:
+                  name: db-bootstrap-secret
+                  key: url
+          command: ["sh","-c"]
+          args:
+            - psql "$DB_URL" -c 'CREATE SCHEMA IF NOT EXISTS admin;'
+chart/templates/00-db-bootstrap-job.yaml
