390f5209c93e91ad031dc1a3257f37923925d225
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-23 00:22:41 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,6 +1,6 @@
-echo "Creating schema '${LIQUIBASE_DEFAULT_SCHEMA_NAME}' if not exists (via liquibase executeSql)..."
-liquibase \
+java -jar /liquibase/liquibase.jar \
   --url="${PF_LIQUIBASE_COMMAND_URL}" \
   --username="${PF_LIQUIBASE_COMMAND_USERNAME}" \
   --password="${PF_LIQUIBASE_COMMAND_PASSWORD}" \
-  executeSql --sql="CREATE SCHEMA IF NOT EXISTS ${LIQUIBASE_DEFAULT_SCHEMA_NAME};"
+  executeSql \
+  --sql="CREATE SCHEMA IF NOT EXISTS ${LIQUIBASE_DEFAULT_SCHEMA_NAME};"
