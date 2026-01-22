e21d5fe790c4b49b0796f07678b84ac1a76070cd
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-22 23:11:24 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,4 +1,14 @@
-nslookup ap27627-docker-pf.repo.artifactory-dogen.group.echonet
-nslookup ap27627-docker-pf.repo.artifactory-dogen.group.echonet.net.intra
-nslookup repo.artifactory-dogen.group.echonet
-nslookup repo.artifactory-dogen.group.echonet.net.intra
+kubectl patch serviceaccount default \
+  -n ns-wall-e-springboot \
+  -p '{"imagePullSecrets":[{"name":"docker-registry-cred"}]}'
+
+
+kubectl get sa default -n ns-wall-e-springboot -o yaml | sed -n '/imagePullSecrets/,+3p'
+
+
+kubectl get job wall-e-db-bootstrap -n ns-wall-e-springboot -o jsonpath='{.spec.template.spec.serviceAccountName}{"\n"}'
+
+
+kubectl patch serviceaccount <NOM_SA> \
+  -n ns-wall-e-springboot \
+  -p '{"imagePullSecrets":[{"name":"docker-registry-cred"}]}'
