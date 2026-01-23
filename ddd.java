f37b789b5dc26c0cd72d5f754820320341d49416
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-23 16:05:38 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,1 +1,12 @@
-{{ include "common-library.hashicorp.vaultenv" (dict "Values" .Values.liquibase "Release" .Release "Chart" .Chart "Capabilities" .Capabilities) | nindent 10 }}
+env:
+  - name: PGHOST
+    value: "postgresql.ns-postgresql.svc.cluster.local"
+  - name: PGPORT
+    value: "5432"
+  - name: PGDATABASE
+    value: "ibmclouddb"
+  - name: PGUSER
+    value: "vault:database/postgres/pg0000000/creds/own_pg0000000_ibmclouddb#username"
+  - name: PGPASSWORD
+    value: "vault:database/postgres/pg0000000/creds/own_pg0000000_ibmclouddb#password"
+{{ include "common-library.hashicorp.vaultenv" (dict "Values" .Values.liquibase "Release" .Release "Chart" .Chart "Capabilities" .Capabilities) | nindent 2 }}
