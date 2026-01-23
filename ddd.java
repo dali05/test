511c58c2321fea8710983235e0e58fb5ab826208
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-23 02:20:24 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,5 +1,21 @@
-env:
-            {{- range .Values.liquibase.job.extraEnv }}
-            - name: {{ .name | quote }}
-              value: {{ .value | quote }}
-            {{- end }}
+args:
+            - |
+              set -e
+              echo "=== DB Bootstrap start ==="
+
+              echo ">>> Create schema admin if not exists (via liquibase executeSql)"
+              vaultenv liquibase \
+                --url="$PF_LIQUIBASE_COMMAND_URL" \
+                --username="$PF_LIQUIBASE_COMMAND_USERNAME" \
+                --password="$PF_LIQUIBASE_COMMAND_PASSWORD" \
+                executeSql --sql="CREATE SCHEMA IF NOT EXISTS admin;"
+
+              echo ">>> Run liquibase update"
+              vaultenv liquibase \
+                --url="$PF_LIQUIBASE_COMMAND_URL" \
+                --username="$PF_LIQUIBASE_COMMAND_USERNAME" \
+                --password="$PF_LIQUIBASE_COMMAND_PASSWORD" \
+                --changeLogFile="$PF_LIQUIBASE_COMMAND_CHANGELOG_FILE" \
+                update
+
+              echo "=== DB Bootstrap done ==="
