a4c4a8ab64adeef6ed128c2f7907cb5f1228e096
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-26 16:54:03 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,3 +1,7 @@
-GRANT CREATE ON DATABASE ibmclouddb TO mon_user;
-
-GRANT USAGE, CREATE ON SCHEMA admin TO mon_user;
+SELECT
+  r1.rolname AS vault_user,
+  r2.rolname AS member_of
+FROM pg_auth_members m
+JOIN pg_roles r1 ON m.member = r1.oid
+JOIN pg_roles r2 ON m.roleid = r2.oid
+WHERE r1.rolname = 'COLLE_ICI_LE_USERNAME_VAULT';
