a2c2a4699a17857f4b23b0364ea02b0788b20809
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-28 21:14:06 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,14 +1,32 @@
-containers:
-  - name: liquibase-bootstrap
-    image: ...
-    env:
-{{ toYaml .Values.liquibase.bootstrap.extraEnv | nindent 6 }}
-
+{{- if .Values.liquibase.bootstrap.enabled }}
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: {{ include "common-library.fullName" . }}-liquibase-bootstrap
+  annotations:
+    "helm.sh/hook": pre-install,pre-upgrade
+    "helm.sh/hook-weight": "-20"
+    "helm.sh/hook-delete-policy": before-hook-creation,hook-succeeded
+spec:
+  backoffLimit: 1
+  template:
+    metadata:
+      labels:
+        app.kubernetes.io/name: {{ include "common-library.fullName" . }}
+    spec:
+      restartPolicy: Never
+      containers:
+        - name: liquibase-bootstrap
+          image: {{ .Values.liquibase.job.image.fullName }}
+          imagePullPolicy: {{ .Values.liquibase.job.image.pullPolicy }}
+          env:
+{{ toYaml .Values.liquibase.bootstrap.extraEnv | nindent 12 }}
 {{- if .Values.liquibase.hashicorp.enabled }}
 {{ include "common-library.hashicorp.vaultenv" (dict
     "Values" .Values.liquibase
     "Release" .Release
     "Chart" .Chart
     "Capabilities" .Capabilities
-  ) | nindent 4 }}
+  ) | nindent 12 }}
+{{- end }}
 {{- end }}
