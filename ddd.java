b3d46700a0632fd4f6943918d15758a66a9473ca
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-29 16:32:42 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,42 +1,9 @@
-apiVersion: batch/v1
-kind: Job
+apiVersion: v1
+kind: ServiceAccount
 metadata:
-  name: {{ .Release.Name }}-db-bootstrap
+  name: local-ap11236-java-application-liquibase
+  namespace: {{ .Release.Namespace }}
   annotations:
     "helm.sh/hook": pre-install,pre-upgrade
     "helm.sh/hook-weight": "-30"
     "helm.sh/hook-delete-policy": before-hook-creation,hook-succeeded
-spec:
-  backoffLimit: 1
-  template:
-    spec:
-      serviceAccountName: local-ap11236-java-application-liquibase
-      restartPolicy: Never
-      containers:
-        - name: db-bootstrap
-          image: postgres:15
-          imagePullPolicy: IfNotPresent
-          command: ["sh", "-c"]
-          args:
-            - |
-              set -e
-              psql -c "CREATE SCHEMA IF NOT EXISTS liquibase_meta;"
-              psql -c "CREATE SCHEMA IF NOT EXISTS admin;"
-          env:
-            - name: PGHOST
-              value: postgresql.ns-postgresql.svc.cluster.local
-            - name: PGPORT
-              value: "5432"
-            - name: PGDATABASE
-              value: ibmclouddb
-            - name: PGUSER
-              value: vault:database/postgres/pg00080000/creds/own_pg00080000_ibmclouddb#username
-            - name: PGPASSWORD
-              value: vault:database/postgres/pg00080000/creds/own_pg00080000_ibmclouddb#password
-
-{{ include "common-library.hashicorp.vaultenv" (dict
-    "Values" .Values
-    "Release" .Release
-    "Chart" .Chart
-    "Capabilities" .Capabilities
-) | nindent 10 }}
