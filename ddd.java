8683f39027ed71038e4edacbd289cfa0d023c244
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-29 15:39:54 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,1 +1,7 @@
-kubectl -n ns-wall-e-springboot get pod wall-e-db-bootstrap-4qst7 -o jsonpath='{.spec.initContainers[*].name}'
+kubectl label namespace ns-wall-e-springboot vault.hashicorp.com/agent-inject=true
+
+kubectl label namespace ns-wall-e-springboot admission.vault.hashicorp.com/enabled=true
+
+
+
+kubectl label namespace ns-wall-e-springboot vault-injection=enabled
