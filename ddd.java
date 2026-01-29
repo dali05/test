e70f0fc250160e18febab38db5d059a0d21e8e9c
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-29 15:46:45 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,1 +1,31 @@
-kubectl -n ns-wall-e-springboot get pod wall-e-db-bootstrap-xxxx -o jsonpath='{.spec.initContainers[*].name}'
+{{- if .Values.liquibase.bootstrap.enabled }}
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: {{ include "common-library.fullName" . }}-liquibase-bootstrap
+  annotations:
+    "helm.sh/hook": pre-install,pre-upgrade
+    "helm.sh/hook-weight": "-20"
+    "helm.sh/hook-delete-policy": before-hook-creation,hook-succeeded
+spec:
+  backoffLimit: 1
+  template:
+    metadata:
+      labels:
+        app.kubernetes.io/name: {{ include "common-library.fullName" . }}
+    spec:
+      serviceAccountName: local-ap11236-java-application-liquibase
+      restartPolicy: Never
+      containers:
+        - name: liquibase-bootstrap
+          image: {{ .Values.liquibase.job.image.fullName }}
+          imagePullPolicy: {{ .Values.liquibase.job.image.pullPolicy }}
+          env:
+{{ toYaml .Values.liquibase.bootstrap.extraEnv | nindent 12 }}
+{{ include "common-library.hashicorp.vaultenv" (dict
+    "Values" .Values.liquibase
+    "Release" .Release
+    "Chart" .Chart
+    "Capabilities" .Capabilities
+  ) | nindent 12 }}
+{{- end }}
