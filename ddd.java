df2e7dc38c7c7c8b67640c1a1592b7e51dc346b7
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-29 16:01:31 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,31 +1,42 @@
-{{- if .Values.liquibase.bootstrap.enabled }}
 apiVersion: batch/v1
 kind: Job
 metadata:
-  name: {{ include "common-library.fullName" . }}-liquibase-bootstrap
+  name: {{ .Release.Name }}-db-bootstrap
   annotations:
     "helm.sh/hook": pre-install,pre-upgrade
-    "helm.sh/hook-weight": "-20"
+    "helm.sh/hook-weight": "-30"
     "helm.sh/hook-delete-policy": before-hook-creation,hook-succeeded
 spec:
   backoffLimit: 1
   template:
-    metadata:
-      labels:
-        app.kubernetes.io/name: {{ include "common-library.fullName" . }}
     spec:
       serviceAccountName: local-ap11236-java-application-liquibase
       restartPolicy: Never
       containers:
-        - name: liquibase-bootstrap
-          image: {{ .Values.liquibase.job.image.fullName }}
-          imagePullPolicy: {{ .Values.liquibase.job.image.pullPolicy }}
+        - name: db-bootstrap
+          image: postgres:15
+          imagePullPolicy: IfNotPresent
+          command: ["sh", "-c"]
+          args:
+            - |
+              set -e
+              psql -c "CREATE SCHEMA IF NOT EXISTS liquibase_meta;"
+              psql -c "CREATE SCHEMA IF NOT EXISTS admin;"
           env:
-{{ toYaml .Values.liquibase.bootstrap.extraEnv | nindent 12 }}
+            - name: PGHOST
+              value: postgresql.ns-postgresql.svc.cluster.local
+            - name: PGPORT
+              value: "5432"
+            - name: PGDATABASE
+              value: ibmclouddb
+            - name: PGUSER
+              value: vault:database/postgres/pg00080000/creds/own_pg00080000_ibmclouddb#username
+            - name: PGPASSWORD
+              value: vault:database/postgres/pg00080000/creds/own_pg00080000_ibmclouddb#password
+
 {{ include "common-library.hashicorp.vaultenv" (dict
-    "Values" .Values.liquibase
+    "Values" .Values
     "Release" .Release
     "Chart" .Chart
     "Capabilities" .Capabilities
-  ) | nindent 12 }}
-{{- end }}
+) | nindent 10 }}
