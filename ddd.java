776056009bceee8180978cf8673ba0d6c8a87c5f
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-30 16:56:56 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,23 +1,59 @@
-job:
-  initContainers:
-    enabled: true
-    containers:
-      - name: init-liquibase-schema
-        image: "<LA_MEME_IMAGE_QUE_LIQUIBASE>"
-        command: ["sh","-c"]
-        args:
-          - >
-            psql "$PF_LIQUIBASE_COMMAND_URL"
-            -U "$PF_LIQUIBASE_COMMAND_USERNAME"
-            -d "$PF_LIQUIBASE_COMMAND_DATABASE"
-            -c "CREATE SCHEMA IF NOT EXISTS liquibase;"
-        env:
-          # Reprendre les mêmes variables que le container Liquibase
-          - name: PF_LIQUIBASE_COMMAND_URL
-            value: "jdbc:postgresql://...."   # ou ta valeur existante
-          - name: PF_LIQUIBASE_COMMAND_USERNAME
-            value: "omm_...@username"         # vaultenv va résoudre
-          - name: PF_LIQUIBASE_COMMAND_PASSWORD
-            value: "omm_...@password"         # vaultenv va résoudre
-          - name: PF_LIQUIBASE_COMMAND_DATABASE
-            value: "..."                      # si vous l’avez
+{{- /*
+Job hook qui crée le schema Liquibase avant l'exécution du job Liquibase
+*/ -}}
+{{- $ctx := dict "Values" .Values.liquibase "Chart" .Chart "Release" .Release "Capabilities" .Capabilities -}}
+
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: {{ include "common-library.fullname" . }}-liquibase-schema
+  annotations:
+    "helm.sh/hook": pre-install, pre-upgrade
+    "helm.sh/hook-weight": "-2"
+    "helm.sh/hook-delete-policy": before-hook-creation,hook-succeeded
+  labels:
+{{ include "common-library.metadata.labels" . | nindent 4 }}
+
+spec:
+  backoffLimit: 1
+  template:
+    metadata:
+      labels:
+{{ include "common-library.selector.labels" . | nindent 8 }}
+{{ include "common-library.deployment.labels" . | nindent 8 }}
+    spec:
+      restartPolicy: Never
+      serviceAccountName: {{ include "common-library.serviceAccountName" . }}
+
+      containers:
+        - name: init-liquibase-schema
+          image: "{{ .Values.liquibase.job.image.fullName }}"
+          imagePullPolicy: {{ .Values.liquibase.job.image.pullPolicy | default "IfNotPresent" }}
+
+          command: ["sh","-c"]
+          args:
+            - |
+              set -euo pipefail
+
+              SCHEMA="${LIQUIBASE_LIQUIBASE_SCHEMA_NAME:-liquibase}"
+
+              JDBC_URL="${PF_LIQUIBASE_COMMAND_URL}"
+              PGURL="$(echo "$JDBC_URL" | sed -E 's|^jdbc:||')"
+
+              export PGPASSWORD="${PF_LIQUIBASE_COMMAND_PASSWORD}"
+
+              echo "Creating schema if not exists: ${SCHEMA}"
+              psql "${PGURL}" -U "${PF_LIQUIBASE_COMMAND_USERNAME}" \
+                -v ON_ERROR_STOP=1 \
+                -c "CREATE SCHEMA IF NOT EXISTS \"${SCHEMA}\";"
+
+          env:
+{{ include "common-library.hashicorp.vaultenv" $ctx | nindent 12 }}
+            - name: PF_LIQUIBASE_COMMAND_URL
+              value: "{{ .Values.liquibase.config.url }}"
+            - name: PF_LIQUIBASE_COMMAND_USERNAME
+              value: "{{ .Values.liquibase.config.username }}"
+            - name: PF_LIQUIBASE_COMMAND_PASSWORD
+              value: "{{ .Values.liquibase.config.password }}"
+            - name: LIQUIBASE_LIQUIBASE_SCHEMA_NAME
+              value: "{{ .Values.liquibase.config.liquibaseSchema }}"
