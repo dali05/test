bfb251af6bba7d64e40b00f98b0413567e8b39ad
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-30 17:16:35 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,45 +1,21 @@
-apiVersion: batch/v1
-kind: Job
-metadata:
-  name: {{ include "common-library.fullname" . }}-liquibase-schema
-  annotations:
-    "helm.sh/hook": pre-install, pre-upgrade
-    "helm.sh/hook-weight": "-2"
-    "helm.sh/hook-delete-policy": before-hook-creation,hook-succeeded
-spec:
-  backoffLimit: 1
-  template:
-    spec:
-      restartPolicy: Never
-      containers:
-        - name: create-schema
-          image: postgres:15-alpine
-          command: ["sh","-c"]
-          args:
-            - |
-              set -e
-              echo "Creating schema liquibase if not exists"
-              export PGPASSWORD="$DB_PASSWORD"
-              psql \
-                -h "$DB_HOST" \
-                -p "$DB_PORT" \
-                -U "$DB_USER" \
-                -d "$DB_NAME" \
-                -c "CREATE SCHEMA IF NOT EXISTS liquibase;"
-          env:
-            - name: DB_HOST
-              value: "<host>"
-            - name: DB_PORT
-              value: "5432"
-            - name: DB_NAME
-              value: "<database>"
-            - name: DB_USER
-              valueFrom:
-                secretKeyRef:
-                  name: <secret>
-                  key: username
-            - name: DB_PASSWORD
-              valueFrom:
-                secretKeyRef:
-                  name: <secret>
-                  key: password
+000-bootstrap-liquibase-schema.xml
+
+  <?xml version="1.0" encoding="UTF-8"?>
+<databaseChangeLog
+  xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="
+    http://www.liquibase.org/xml/ns/dbchangelog
+    http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-4.32.xsd">
+
+  <changeSet id="000-create-liquibase-schema" author="liquibase">
+    <sql>
+      CREATE SCHEMA IF NOT EXISTS liquibase;
+    </sql>
+  </changeSet>
+
+</databaseChangeLog>
+
+
+<include file="db/changelog/000-bootstrap-liquibase-schema.xml"
+         relativeToChangelogFile="true"/>
