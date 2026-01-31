602fa8eead4e825e29efff994f5f5b7a52d5e67e
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-31 18:46:47 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,1 +1,14 @@
-helm template wall-e chart -f chart-env/local/values.yaml | grep -n "vault-agent-config"
+{{- if and (.Values.hashicorp.enabled) (eq .Values.hashicorp.method "vault-agent-initcontainer") }}
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: {{ include "common-library.fullname" . }}-vault-agent-config
+  annotations:
+    "helm.sh/hook": pre-install,pre-upgrade
+    "helm.sh/hook-weight": "-20"
+    "helm.sh/hook-delete-policy": before-hook-creation
+data:
+  vault-agent-config.hcl: |
+{{- /* On injecte le contenu généré par le helper dans la clé data */ -}}
+{{- /* Si ton helper crée déjà un ConfigMap complet, alors utilise Option 2 ci-dessous */ -}}
+{{- end }}
