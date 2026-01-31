39de37a634004fa382941b3c4e4ce2877a24df8c
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-31 18:29:27 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,20 +1,10 @@
-configmap:
-  enabled: false
+{{- $ctx := deepCopy $ -}}
+{{- $_ := set $ctx.Values "configmap" (dict "enabled" false) -}}
 
-hashicorp:
-  enabled: true
-  method: vault-agent-initcontainer
-  ns: root
-  path: kubernetes_kub00001_local
-  approle: ns-wall-e-springboot-local-ap11236-java-application-liquibase
-  template: |
-    template {
-      destination = "/etc/secrets/pg.env"
-      perms       = "0600"
-      contents = <<EOH
-{{- with secret "database/postgres/pg0000000/creds/own_pg0000000_ibmclouddb" -}}
-export PGUSER="{{ .Data.username }}"
-export PGPASSWORD="{{ .Data.password }}"
-{{- end -}}
-EOH
-    }
+initContainers:
+  {{- include "common-library.hashicorp.initcontainer" $ctx | nindent 8 }}
+
+...
+
+volumes:
+  {{- include "common-library.hashicorp.initcontainer.volumes" $ctx | nindent 8 }}
