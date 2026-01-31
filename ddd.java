1da7306f09565c42bd505df3769be1622888376a
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-31 18:12:04 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,5 +1,20 @@
-initContainers:
-  {{- include "common-library.hashicorp.initcontainer" . | nindent 8 }}
+configmap:
+  enabled: false
 
-volumes:
-  {{- include "common-library.hashicorp.initcontainer.volumes" . | nindent 8 }}
+hashicorp:
+  enabled: true
+  method: vault-agent-initcontainer
+  ns: root
+  path: kubernetes_kub00001_local
+  approle: ns-wall-e-springboot-local-ap11236-java-application-liquibase
+  template: |
+    template {
+      destination = "/etc/secrets/pg.env"
+      perms       = "0600"
+      contents = <<EOH
+{{- with secret "database/postgres/pg0000000/creds/own_pg0000000_ibmclouddb" -}}
+export PGUSER="{{ .Data.username }}"
+export PGPASSWORD="{{ .Data.password }}"
+{{- end -}}
+EOH
+    }
