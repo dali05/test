800d1cf68cea129de06aa4c4b2439fc69970af09
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-31 19:06:20 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,14 +1,4 @@
-{{- if and (.Values.hashicorp.enabled) (eq .Values.hashicorp.method "vault-agent-initcontainer") }}
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: {{ include "common-library.fullname" . }}-vault-agent-config
-  annotations:
-    "helm.sh/hook": pre-install,pre-upgrade
-    "helm.sh/hook-weight": "-20"
-    "helm.sh/hook-delete-policy": before-hook-creation
-data:
-  vault-agent-config.hcl: |
-{{- /* On injecte le contenu généré par le helper dans la clé data */ -}}
-{{- /* Si ton helper crée déjà un ConfigMap complet, alors utilise Option 2 ci-dessous */ -}}
-{{- end }}
+hashicorp:
+  image:
+    repository: docker-registry-devops.pf.echonet/hashicorp/vault
+    tag: "1.15.2"
