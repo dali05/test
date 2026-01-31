cae68fcfcd1a0a55748f177241bb82b9746bf877
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-01-31 04:22:20 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,76 +1,10 @@
-liquibase:
-  hashicorp:
-    enabled: true
-    method: "vault-agent-initcontainer"
-    addr: "http://vault.ns-vault.svc.cluster.local:8200"
-    ns: ""
+set -euo pipefail
 
-    template: |
-      exit_after_auth = true
-      pid_file = "/home/vault/pidfile"
+DB_HOST="$(echo "$PF_LIQUIBASE_COMMAND_URL" | sed -E 's|^jdbc:postgresql://([^:/]+).*|\1|')"
+DB_PORT="$(echo "$PF_LIQUIBASE_COMMAND_URL" | sed -E 's|.*:([0-9]+)/.*|\1|')"
+DB_NAME="$(echo "$PF_LIQUIBASE_COMMAND_URL" | sed -E 's|.*/([^/?]+).*|\1|')"
 
-      auto_auth {
-        method "kubernetes" {
-          mount_path = "auth/kubernetes"
-          config = {
-            role = "ns-wall-e-springboot-local-ap11236-java-application-liquibase"
-          }
-        }
-      }
+export PGPASSWORD="$PF_LIQUIBASE_COMMAND_PASSWORD"
 
-      sink "file" {
-        config = {
-          path = "/home/vault/.token"
-        }
-      }
-
-      template {
-        contents = <<EOT
-DB_USER={{ with secret "database/postgres/pg000000/creds/app_pg000000_ibmclouddb" }}{{ .Data.username }}{{ end }}
-DB_PASSWORD={{ with secret "database/postgres/pg000000/creds/app_pg000000_ibmclouddb" }}{{ .Data.password }}{{ end }}
-DB_HOST=postgresql.ns-postgresql.svc.cluster.local
-DB_PORT=5432
-DB_NAME=<TA_DB>
-EOT
-        destination = "/applis/vault/db.env"
-      }
-
-
-apiVersion: batch/v1
-kind: Job
-metadata:
-  name: {{ include "common-library.fullname" . }}-db-bootstrap
-  annotations:
-    "helm.sh/hook": pre-install, pre-upgrade
-    "helm.sh/hook-weight": "-3"
-    "helm.sh/hook-delete-policy": before-hook-creation,hook-succeeded
-spec:
-  backoffLimit: 1
-  template:
-    spec:
-      restartPolicy: Never
-      serviceAccountName: {{ include "common-library.serviceAccountName" . }}
-
-      containers:
-        - name: create-liquibase-schema
-          image: "{{ .Values.liquibase.job.image.fullName }}"
-          imagePullPolicy: {{ .Values.liquibase.job.image.pullPolicy | default "IfNotPresent" }}
-          command: ["sh","-c"]
-          args:
-            - |
-              set -euo pipefail
-
-              echo "Sourcing Vault DB env"
-              . /applis/vault/db.env
-
-              echo "Creating schema liquibase..."
-              psql \
-                -h "$DB_HOST" \
-                -p "$DB_PORT" \
-                -U "$DB_USER" \
-                -d "$DB_NAME" \
-                -v ON_ERROR_STOP=1 \
-                -c "CREATE SCHEMA IF NOT EXISTS liquibase;"
-          volumeMounts:
-            - name: vault-shared-data
-              mountPath: /applis/vault
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$PF_LIQUIBASE_COMMAND_USERNAME" -d "$DB_NAME" \
+  -c "CREATE SCHEMA IF NOT EXISTS liquibase;"
