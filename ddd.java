f08c3011b26c943c1786809e3b6459b44e9892d9
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-02-01 06:19:11 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,8 +1,64 @@
-template:
-      - destination: "/etc/secrets/pg.env"
-        perms: "0600"
-        contents: |
-          {{- with secret "database/postgres/pg0000000/creds/own_pg0000000_ibmclouddb" -}}
-          export PGUSER="{{ .Data.username }}"
-          export PGPASSWORD="{{ .Data.password }}"
-          {{- end -}}
+{{- /*
+templates/00-vault-agent-configmap.yaml
+Crée la ConfigMap qui contient /etc/vault/vault-agent-config.hcl
+*/ -}}
+
+{{- if and (.Values.dbBootstrap.hashicorp.enabled) (eq .Values.dbBootstrap.hashicorp.method "vault-agent-initcontainer") -}}
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: {{ .Release.Name }}-vault-agent-config
+  namespace: {{ .Release.Namespace }}
+  labels:
+    app.kubernetes.io/name: {{ .Chart.Name }}
+    app.kubernetes.io/instance: {{ .Release.Name }}
+data:
+  vault-agent-config.hcl: |-
+    exit_after_auth = true
+    pid_file        = "/home/vault/pidfile"
+
+    vault {
+      address = "{{ required "dbBootstrap.hashicorp.vaultAddr is required" .Values.dbBootstrap.hashicorp.vaultAddr }}"
+    }
+
+    # Un seul auto_auth (sinon Vault Agent plante)
+    auto_auth {
+      method "kubernetes" {
+        mount_path = "{{ required "dbBootstrap.hashicorp.path is required" .Values.dbBootstrap.hashicorp.path }}"
+        config = {
+          role = "{{ required "dbBootstrap.hashicorp.approle is required" .Values.dbBootstrap.hashicorp.approle }}"
+        }
+      }
+      sink "file" {
+        config = {
+          path = "/home/vault/.vault-token"
+        }
+      }
+    }
+
+    # Recommandé : réutiliser le token auto_auth
+    cache {
+      use_auto_auth_token = true
+    }
+
+    # Nécessaire si tu actives des features qui exigent un listener (api_proxy, etc.)
+    listener "tcp" {
+      address     = "127.0.0.1:8200"
+      tls_disable = true
+    }
+
+    template_config {
+      exit_on_retry_failure = true
+    }
+
+    {{- $templates := .Values.dbBootstrap.hashicorp.template | default (list) -}}
+    {{- range $i, $tpl := $templates }}
+    template {
+      destination = "{{ required (printf "dbBootstrap.hashicorp.template[%d].destination is required" $i) $tpl.destination }}"
+      perms       = "{{ $tpl.perms | default "0600" }}"
+      contents = <<EOH
+{{- required (printf "dbBootstrap.hashicorp.template[%d].contents is required" $i) $tpl.contents | nindent 0 }}
+EOH
+    }
+    {{- end }}
+{{- end }}
