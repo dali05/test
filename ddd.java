2fc259144b0b768149bb5fca84f32fb1cbbc6336
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-02-01 21:57:30 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,3 +1,6 @@
-kubectl exec -n ns-vault -it vault-0 -- sh
-vault login <TON_TOKEN_ADMIN>
-vault auth list
+kubectl exec -n ns-vault -it vault-0 -- sh -lc '
+export VAULT_ADDR=http://127.0.0.1:8200
+export VAULT_NAMESPACE=ns-wall-e-springboot
+vault login <TON_TOKEN>
+vault read auth/kubernetes_kube001_local/role/ns-wall-e-springboot-local-ap11236-java-application-liquibase
+'
