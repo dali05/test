473607d4113d76d66b61ec0749dea5e17d432edd
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-02-02 03:09:58 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,29 +1,10 @@
-args:
-  - |
-    set -e
-
-    echo "Waiting for Vault-rendered /etc/secrets/pg.env..."
-    i=0
-    while [ ! -s /etc/secrets/pg.env ] && [ $i -lt 120 ]; do
-      i=$((i+1))
-      sleep 1
-    done
-
-    if [ ! -s /etc/secrets/pg.env ]; then
-      echo "ERROR: /etc/secrets/pg.env missing or empty"
-      ls -la /etc/secrets || true
-      exit 1
-    fi
-
-    echo "OK: pg.env exists. Loading it..."
-    set -a
-    . /etc/secrets/pg.env
-    set +a
-
-    echo "PGUSER=$PGUSER"
-    echo "PGPASSWORD is set? $([ -n "$PGPASSWORD" ] && echo yes || echo no)"
-
-    # exemple psql
-    export PGPASSWORD="$PGPASSWORD"
-    psql -h "$PGHOST" -p "$PGPORT" -U "$PGUSER" -d "$PGDATABASE" \
-      -c "CREATE SCHEMA IF NOT EXISTS admin; CREATE SCHEMA IF NOT EXISTS liquibase;"
+template {
+  destination = "/etc/secrets/pg.env"
+  perms       = "0600"
+  contents = <<EOH
+{{`{{- with secret "database/postgres/pg0000000/creds/own_pg0000000_ibmclouddb" -}}`}}
+export PGUSER="{{`{{ .Data.username }}`}}"
+export PGPASSWORD="{{`{{ .Data.password }}`}}"
+{{`{{- end -}}`}}
+EOH
+}
