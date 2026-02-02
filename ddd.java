a10c8b5faffbe4e926e1cd33840e8d36c239120c
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-02-02 03:55:49 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,1 +1,3 @@
-helm get manifest wall-e -n ns-wall-e-springboot | nl -ba | sed -n '90,140p'
+kubectl config current-context
+helm get manifest wall-e -n ns-wall-e-springboot | head
+kubectl get ns | grep ns-wall-e-springboot
