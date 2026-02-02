c5beb23bc6f146bc4e3c3d7ee796f1828699e12d
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-02-02 03:56:05 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,3 +1,6 @@
 kubectl config current-context
 helm get manifest wall-e -n ns-wall-e-springboot | head
 kubectl get ns | grep ns-wall-e-springboot
+
+
+kubectl get cm -A | grep wall-e-vault-agent-config
