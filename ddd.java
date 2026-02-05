26e85e1c836f837e0fbe1c9c7b9cd06ce3e353e8
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-02-05 23:19:39 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -2,15 +2,22 @@ apiVersion: batch/v1
 kind: Job
 metadata:
   name: postgres-test-job
-  namespace: dev   # ⚠️ change si besoin
+  namespace: dev
 spec:
   backoffLimit: 0
   template:
     spec:
       restartPolicy: Never
+
+      securityContext:
+        seccompProfile:
+          type: RuntimeDefault
+
       containers:
         - name: postgres
           image: postgres:15
+          securityContext:
+            allowPrivilegeEscalation: false
           env:
             - name: POSTGRES_PASSWORD
               value: example
