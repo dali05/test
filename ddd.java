5700004a0914b6db15207cb9185642ef78cac53e
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-02-05 23:21:19 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -2,17 +2,28 @@ apiVersion: batch/v1
 kind: Job
 metadata:
   name: postgres-test-job
-  namespace: dev
+  namespace: ns002i012773
+  annotations:
+    com.illumio.app: postgres-test
+    com.illumio.env: dev
+    com.illumio.app-tier: database
+    com.illumio.middleware: postgres
+    com.illumio.role: job
 spec:
   backoffLimit: 0
   template:
+    metadata:
+      annotations:
+        com.illumio.app: postgres-test
+        com.illumio.env: dev
+        com.illumio.app-tier: database
+        com.illumio.middleware: postgres
+        com.illumio.role: job
     spec:
       restartPolicy: Never
-
       securityContext:
         seccompProfile:
           type: RuntimeDefault
-
       containers:
         - name: postgres
           image: postgres:15
