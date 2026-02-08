a6cd7e6f5467d6f618e7c643ac422007ecfb3473
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-02-08 17:52:59 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,24 +1,3 @@
-apiVersion: v1
-kind: Pod
-metadata:
-  name: postgres-test-pod
-  namespace: ns002i012773
-  annotations:
-    com.illumio.app: postgres-test
-    com.illumio.env: dev
-    com.illumio.app-tier: database
-    com.illumio.middleware: postgres
-    com.illumio.role: test
-spec:
-  restartPolicy: Never
-  securityContext:
-    seccompProfile:
-      type: RuntimeDefault
-  containers:
-    - name: postgres
-      image: postgres:15
-      securityContext:
-        allowPrivilegeEscalation: false
-      env:
-        - name: POSTGRES_PASSWORD
-          value: example
+sudo mkdir -p /etc/docker/certs.d/base-images.repo.artifactory-dogen.group.echonet
+sudo cp artifactory-dogen-ca.crt \
+ /etc/docker/certs.d/base-images.repo.artifactory-dogen.group.echonet/ca.crt
