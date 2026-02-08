75cc2bd996fb84533bf470a50a50520661f88e7f
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-02-08 17:57:50 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,3 +1,5 @@
-sudo mkdir -p /etc/docker/certs.d/base-images.repo.artifactory-dogen.group.echonet
-sudo cp artifactory-dogen-ca.crt \
- /etc/docker/certs.d/base-images.repo.artifactory-dogen.group.echonet/ca.crt
+{
+  "insecure-registries": [
+    "base-images.repo.artifactory-dogen.group.echonet"
+  ]
+}
