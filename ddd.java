d22b469d11bc2cc63931e15740ee8d453e14e17f
mohamedAliLaaroussi <dali.laaroussi@gmail.com>
2026-02-10 17:23:50 +0100

    Update ddd.java

--- ddd.java
+++ ddd.java
@@ -1,5 +1,23 @@
-{
-  "insecure-registries": [
-    "base-images.repo.artifactory-dogen.group.echonet"
-  ]
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+  @Bean
+  public GroupedOpenApi accessApi() {
+    return GroupedOpenApi.builder()
+        .group("access")
+        .packagesToScan("com.tonprojet.access.controller")
+        .build();
+  }
+
+  @Bean
+  public GroupedOpenApi callbackApi() {
+    return GroupedOpenApi.builder()
+        .group("callback")
+        .packagesToScan("com.tonprojet.callback.controller")
+        .build();
+  }
 }
